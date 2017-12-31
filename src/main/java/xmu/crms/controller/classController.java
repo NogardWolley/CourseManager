5cf440c9b8b4968ded416d1b7dbb39fcf218ff09
hj <1677116881@qq.com>
2017-12-31 22:46:35 +0800

    更新 Classcontroller 缺少小组选删topic

--- src/main/java/xmu/crms/controller/classController.java
+++ src/main/java/xmu/crms/controller/classController.java
@@ -1,14 +1,14 @@
 package xmu.crms.controller;
 
+import io.jsonwebtoken.lang.Classes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import xmu.crms.entity.ClassInfo;
+import xmu.crms.entity.FixGroup;
 import xmu.crms.entity.User;
 import xmu.crms.exception.CourseNotFoundException;
 import xmu.crms.exception.UserNotFoundException;
@@ -16,13 +16,16 @@ import xmu.crms.service.ClassService;
 import xmu.crms.service.FixGroupService;
 import xmu.crms.service.SeminarGroupService;
 import xmu.crms.service.UserService;
-import xmu.crms.vo.ClassVO;
-
+import xmu.crms.vo.*;
+import xmu.crms.exception.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /**
  * @author hj
@@ -70,7 +73,7 @@ public class classController {
             } catch (Exception e) {
 
             } finally {
-                List<ClassVO> userClassVOS = new ArrayList<>();
+                List<UserClassVO> userClassVOS = new ArrayList<>();
                 //获取所有课程里没有我所选的班级的课程
                 //有选课信息
                 if (myClassInfos != null) {
@@ -104,16 +107,300 @@ public class classController {
                 return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(userClassVOS);
             }
 
-        } catch (UserNotFoundException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(404).build();
-        } catch (CourseNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(404).build();
         }
     }
 
+    /**
+     *按照ID修改班级
+     *
+     * @author hj
+     * @date 2017年12月31日
+     */
+    @PreAuthorize("hasRole('TEACHER')")
+    @RequestMapping(value = "/{classId}", method = PUT)
+    @ResponseBody
+    public ResponseEntity updateClassById(@PathVariable BigInteger classId,
+                                          HttpServletRequest httpServletRequest) throws IOException {
+        //将JSON转换成对象
+        BufferedReader br = httpServletRequest.getReader();
+        BigInteger userId = (BigInteger) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String str, wholeStr = "";
+        while ((str = br.readLine()) != null) {
+            wholeStr += str;
+        }
+        ClassCreateVO classCreateVO = new ClassCreateVO(wholeStr);
+        ClassInfo classInfo = new ClassInfo(classCreateVO);
 
+        try {
+            classService.updateClassByClassId(classId, classInfo);
+            return ResponseEntity.status(204).build();
+        } catch (ClassesNotFoundException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    /**
+     *按ID删除班级
+     *
+     * @author hj
+     * @date 12月31日
+     */
+    @PreAuthorize("hasRole('TEACHER')")
+    @RequestMapping(value = "/{classId}", method = DELETE)
+    @ResponseBody
+    public ResponseEntity deleteClassById(@PathVariable BigInteger classId) {
+        try {
+            classService.deleteClassByClassId(classId);
+            return ResponseEntity.status(204).build();
+        } catch (ClassesNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    /**
+     *按ID查找学生列表（查询学号、姓名开头）
+     *
+     * @author hj
+     * @date 12月31日
+     */
+    @PreAuthorize("hasRole('TEACHER') or hasRole('STUDENT')")
+    @RequestMapping(value = "/{classId}/student", method = GET)
+    @ResponseBody
+    public ResponseEntity getStudentListByClassId(@PathVariable BigInteger classId,
+                                                  @RequestParam(value = "numberBeginWith", required = false) String numberBeginWith,
+                                                  @RequestParam(value = "nameBeginWith", required = false) String nameBeginWith) {
+        List<User> list = new ArrayList<User>();
+        try {
+            System.out.println(numberBeginWith + ':' + nameBeginWith);
+            list = userService.listUserByClassId(classId, numberBeginWith, nameBeginWith);
+        } catch (ClassesNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (list.isEmpty()) {
+            return ResponseEntity.status(404).body(null);
+        } else {
+            List<ClassStudentVO> listStudent = new ArrayList<ClassStudentVO>();
+            for (int i = 0; i < list.size(); i++) {
+                ClassStudentVO classStudentVO = new ClassStudentVO(list.get(i).getId(), list.get(i).getName(), list.get(i).getNumber());
+                listStudent.add(classStudentVO);
+            }
+            return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(listStudent);
+        }
+    }
+
+
+/**
+ *学生按ID选择班级
+ *
+ * @author hj
+ * @date 12月31日
+ */
+    @PreAuthorize("hasRole('STUDENT')")
+    @RequestMapping(value = "/{classId}/student", method = POST)
+    @ResponseBody
+    public ResponseEntity selectClass(@PathVariable BigInteger classId) {
+        BigInteger userId = (BigInteger) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        try {
+            classService.insertCourseSelectionById(userId, classId);
+            return ResponseEntity.status(204).build();
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (ClassesNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    /**
+     *  学生按ID取消选择班级
+     *
+     * @author hj
+     * @date 12月31日
+     */
+    @PreAuthorize("hasRole('STUDENT')")
+    @RequestMapping(value = "/{classId}/student/{studentId}", method = DELETE)
+    @ResponseBody
+    public ResponseEntity deSelectClass(@PathVariable BigInteger classId, @PathVariable BigInteger studentId) {
+        try {
+            classService.deleteCourseSelectionById(studentId, classId);
+            return ResponseEntity.status(204).build();
+        } catch (ClassesNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+
+
+    /**
+     *按ID获取自身所在班级小组
+     *
+     * @author YellowPure
+     * @date 11:07 2017/12/27
+     */
+    @PreAuthorize("hasRole('STUDENT')")
+    @RequestMapping(value = "/{classId}/classgroup", method = GET)
+    @ResponseBody
+    public ResponseEntity getGroupByClassId(@PathVariable BigInteger classId) {
+
+        BigInteger userId = (BigInteger) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        try {
+            FixGroup fixGroup = fixGroupService.getFixedGroupById(userId, classId);
+            List<User> members = fixGroupService.listFixGroupMemberByGroupId(fixGroup.getId());
+            System.out.println(members.toString());
+            FixGroupVO fixGroupVO = new FixGroupVO(fixGroup, members);
+            return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(fixGroupVO);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (ClassesNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (FixGroupNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    /**
+     *
+     班级小组组长辞职
+     *
+     * @author hj
+     * @date 12月31日
+            */
+    @PreAuthorize("hasRole('STUDENT')")
+    @RequestMapping(value = "/{classId}/classgroup/resign", method = PUT)
+    public ResponseEntity resignFromGroup(@PathVariable BigInteger classId) {
+        BigInteger userId = (BigInteger) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        try {
+            FixGroup fixGroup = fixGroupService.getFixedGroupById(userId, classId);
+            seminarGroupService.resignLeaderById(fixGroup.getId(), userId);
+            return ResponseEntity.status(204).build();
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (ClassesNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (GroupNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (InvalidOperationException e) {
+            return ResponseEntity.status(403).build();
+        }
+    }
+
+    /**
+     *成为班级小组组长
+     *
+     * @author hj
+     * @date 12月31日
+     */
+    @PreAuthorize("hasRole('TEACHER') or hasRole('STUDENT')")
+    @RequestMapping(value = "/{classId}/classgroup/assign", method = PUT)
+    public ResponseEntity assignFromGroup(@PathVariable BigInteger classId) {
+        BigInteger userId = (BigInteger) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        try {
+            FixGroup fixGroup = fixGroupService.getFixedGroupById(userId, classId);
+            seminarGroupService.assignLeaderById(fixGroup.getId(), userId);
+            return ResponseEntity.status(204).build();
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (ClassesNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (GroupNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (InvalidOperationException e) {
+            return ResponseEntity.status(403).build();
+        }
+    }
+
+    /**
+     *添加班级小组成员
+     *
+     * @author hj
+     * @date 12月31日
+     */
+    @PreAuthorize("hasRole('TEACHER') or hasRole('STUDENT')")
+    @RequestMapping(value = "/{classId}/classgroup/add", method = PUT)
+    public ResponseEntity addMemberInGroup(@PathVariable BigInteger classId, @RequestBody UserVO user) {
+        BigInteger userId = user.getId();
+        try {
+            /*每个人一开始都是fixGroup的leader， 若插入的时候组内还只有一个人，
+            说明目前的组是个虚拟的组，要现在数据库里面插入一个fixGroup的记录
+			 */
+            BigInteger groupId = BigInteger.valueOf(0);
+            FixGroup fixGroup = fixGroupService.getFixedGroupById(userId, classId);
+
+            //如果没有小组，创建固定分组，设置班级信息，组长信息
+            if (fixGroup == null) {
+                ClassInfo classInfo = new ClassInfo();
+                classInfo.setId(classId);
+                User leader = new User();
+                leader.setId(userId);
+                FixGroup fixGroup1 = new FixGroup();
+                fixGroup1.setClassInfo(classInfo);
+                fixGroup1.setLeader(leader);
+                fixGroupService.insertFixGroupByClassId(userId, classId);
+            }
+            fixGroupService.insertStudentIntoGroup(userId, groupId);
+            return ResponseEntity.status(204).build();
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (ClassesNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (InvalidOperationException e) {
+            return ResponseEntity.status(403).build();
+        } catch (FixGroupNotFoundException e) {
+            return ResponseEntity.status(403).build();
+        }
+    }
+
+    /**
+     *删除组内成员
+     *
+     * @author hj
+     * @date 12月31日
+     */
+    @PreAuthorize("hasRole('TEACHER') or hasRole('STUDENT')")
+    @RequestMapping(value = "/{classId}/classgroup/remove", method = PUT)
+    public ResponseEntity deleteMemberFromGroup(@PathVariable BigInteger classId,
+                                                @RequestBody UserVO userVO) {
+        BigInteger userId = (BigInteger) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        try {
+            //获取小组
+            FixGroup fixGroup = fixGroupService.getFixedGroupById(userId, classId);
+            //删除小组成员
+            fixGroupService.deleteFixGroupUserById(fixGroup.getId(), userVO.getId());
+            return ResponseEntity.status(204).build();
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (FixGroupNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        } catch (ClassesNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+//    /**
+//     *班级小组按ID选择话题
+//     * @author hj
+//     * @date 12月31日
+//     */
+//    @PreAuthorize(" hasRole('STUDENT')")
+//    @RequestMapping(value = "/{classId}/classgroup/remove", method = POST)
+//    public  ResponseEntity chooseGroopTopic (@PathVariable Integer groupId,
+//                                             @RequestBody TopicVO topicId){
+//
+//
+//
+//    }
+//
+//
+//
+//
+////    班级小组按删除话题
+//
+//
 
 
 }
