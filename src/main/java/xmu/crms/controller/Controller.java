6dda85806b0feb825dd4b4a4d07935a44635049c
NogardWolley <YellowDragon@users.noreply.github.com>
2017-12-31 18:05:04 +0800

    Revert "nothing"
    This reverts commit 27b6deacd11d2570fac7b5babdf3e3807063c2bb.

--- src/main/java/xmu/crms/controller/Controller.java
+++ src/main/java/xmu/crms/controller/Controller.java
@@ -22,15 +22,11 @@ public class Controller {
 	private FixGroupMapper fixGroupMapper;
 
 	@GetMapping("/me")
-	public ResponseEntity<FixGroup> getInfo(){
+	public ResponseEntity<Course> getInfo(){
 		System.out.println("****");
 		BigInteger a,b;
 		a=BigInteger.valueOf(3);b=BigInteger.valueOf(1);
-<<<<<<< HEAD
-			return new ResponseEntity<FixGroup>(fixGroupMapper.getFixedGroupById(a,b),HttpStatus.OK);
-=======
 			return new ResponseEntity<Course>(courseMapper.getCourseByCourseId(b),HttpStatus.OK);
->>>>>>> 7ddd08e71853b136f5f9d389181569dde30470a3
     }
 //	@PutMapping("/me")
 //	public ResponseEntity updateUser(){
