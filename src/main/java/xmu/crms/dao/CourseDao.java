9a535287e130cfa0f94dcc9a7a9a34d5e86bf0c0
NogardWolley <YellowDragon.xmu@users.noreply.github.com>
2017-12-31 15:44:24 +0800

    nothing

--- src/main/java/xmu/crms/dao/CourseDao.java
+++ src/main/java/xmu/crms/dao/CourseDao.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class CourseDao {
     @Autowired
     CourseMapper courseMapper;
+
     public List<Course> listCourseByUserId(BigInteger userId)
         throws IllegalArgumentException,CourseNotFoundException{
         if(!(userId.intValue()>0)){
